986e272e6df53ce2623991fe5b2e7b1f54851afa
Shreyash0002 <108492496+Shreyash0002@users.noreply.github.com>
2022-11-15 22:24:54 +0530

    Time: 0 ms (100.00%), Space: 50.3 MB (18.06%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     //function to find depth of tree on left side
+    int leftDepth(TreeNode node){
+        int depth=0;
+        while(node!=null){
+            node=node.left;
+            depth++;
+        }
+        return depth;
+    }
+    
+    //function to find depth of tree on right side
+    int rightDepth(TreeNode node){
+        int depth=0;
+        while(node!=null){
+            node=node.right;
+            depth++;
+        }
+        return depth;
+    }
+    
+    //main function
+    public int countNodes(TreeNode root) {
+        
+        int left=leftDepth(root);
+        int right=rightDepth(root);
+        
+        //if both the leftmost and right most depth are equal that means
+		//its perfect tree so no. of nodes would be 2^h-1
+        if(left==right){
+            return (int)Math.pow(2,left)-1;
+        }
+        //otherwise it will call count function for left and right subtrees
+		//and add 1 for root node
+        return 1+countNodes(root.left)+countNodes(root.right);
+        
+    }
+}
